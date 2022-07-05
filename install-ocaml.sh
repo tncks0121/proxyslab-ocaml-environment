759c234f1b305031fa57c5e5067c1394eb32aa6e
seungwan.kwon <seungwan.kwon@prosys.kr>
2022-07-05 12:20:04 +0900

    [ocaml] disable sandboxing to suppress warning

--- install-ocaml.sh
+++ install-ocaml.sh
@@ -4,7 +4,7 @@ export OCAML_VERSION=4.13.1
 export OPAM_SWITCH=prosyslab-classroom-$OCAML_VERSION
 export OPAMYES=1
 
-opam init
+opam init --compiler=$OCAML_VERSION --disable-sandboxing
 opam switch create $OPAM_SWITCH --package=ocaml-variants.$OCAML_VERSION+options,ocaml-option-flambda
 
 eval $(opam env)
