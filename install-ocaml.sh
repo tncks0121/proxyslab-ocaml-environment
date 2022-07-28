20d6201ba04e7aa01aff3de12954553d70f3c232
Hhro <seungwan.kwon@prosys.kr>
2022-07-28 14:26:03 +0900

    [ocaml] remove ppx_deriving

--- install-ocaml.sh
+++ install-ocaml.sh
@@ -8,7 +8,7 @@ opam init --compiler=$OCAML_VERSION --disable-sandboxing
 opam switch create $OPAM_SWITCH --package=ocaml-variants.$OCAML_VERSION+options,ocaml-option-flambda
 
 eval $(opam env)
-opam install -y utop dune llvm.13.0.0 ounit merlin ocamlformat ocaml-lsp-server odoc z3 ocamlgraph core ppx_deriving
+opam install -y utop dune llvm.13.0.0 ounit merlin ocamlformat ocaml-lsp-server odoc z3 ocamlgraph core
 opam pin add git+https://github.com/prosyslab-classroom/llvmutils.git
 opam pin add git+https://github.com/prosyslab/cil
 
